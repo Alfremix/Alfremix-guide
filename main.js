835c8231f6171de3f763395171c96bb2c8f51573
Alfremix <88783046+Alfremix@users.noreply.github.com>
2021-11-15 12:32:55 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -6,4 +6,10 @@ function change(){
 	location.href = "courses.html";
 }
 
+function stopmusic(){
+	document.getElementById("classical-music").stop();
+}
+
+document.getElementById("classical-music").play();
+
 
